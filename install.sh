6a8c6990701b27ee1dd0b2dfcd063d8d3a5fdce7
mrstockis <mickem90@gmail.com>
2017-02-18 18:10:11 +0100

    changed shell (back) to bash|added alias-request to install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 clear
 
 mkdir ~/.webSound/
-printf "\n Created folder  ~/.webSound\n"
+printf "\n Created folder  '~/.webSound'\n"
 
 touch ~/.webSound/1
-echo " Added default playlist, called  1"
+echo " Added default playlist, called  '1'"
 
 #adds three items to playlist 1
 printf "\n#Soundcloud\nhttps://soundcloud.com/themidnightofficial/vampires\n" >> ~/.webSound/1
@@ -17,8 +17,14 @@ cp webSound.sh ~/.webSound/webSound.sh
 echo " Copied  webSound.sh"
 cp help ~/.webSound/help
 echo " Copied  help"
-#echo "alias WebSound=~/.webSound/./webSound.sh" >> ~/.bashrc
+printf "\n\033[1;33m Add alias 'WebSound' to ~/.bashrc? y/N: "
+read A
+if [[ $A == "y" ]]; then
+#printf "\nalias WebSound=~/.webSound/./webSound.sh\n" >> ~/.bashrc
 printf "\n\033[1;33m Alias  WebSound  was added to  ~/.bashrc  and will take effect next reboot\n"
+else
+printf "\n Skipped creating alias\n"
+fi
 printf " Script  webSound.sh  is found in  ~/.webSound/ \033[0m\n"
 printf "\n Done, you can now delete this folder\n"
 
--- webSound.sh
+++ webSound.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 N=1
 Folder=~/.webSound/
