287d54b6f7462dfa5a3c8fe3b5dab748c1f9a1d9
mrstockis <mickem90@gmail.com>
2017-03-01 16:35:37 +0100

    cleaned and edited format

--- install.sh
+++ install.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 clear
-
+printf "\n\033[32m -- Installation --\033[0m\n"
 mkdir ~/.webSound/
-printf "\n Created folder  '~/.webSound'\n"
+printf "\n Created folder: ~/.webSound\n"
 
 touch ~/.webSound/1
-echo " Added default playlist, called  '1'"
+echo " Added default playlist: 1"
 
-#adds three items to playlist 1
+## adds three items to playlist 1
 printf "\n#Soundcloud\nhttps://soundcloud.com/themidnightofficial/vampires\n" >> ~/.webSound/1
 printf "\n#Youtube\nhttps://www.youtube.com/watch?v=fWRISvgAygU\n" >> ~/.webSound/1
 printf "\n#Radio\nhttp://109.123.116.202:8010/stream\n" >> ~/.webSound/1
 
-chmod +x webSound.sh
 cp webSound.sh ~/.webSound/webSound.sh
-echo " Copied  webSound.sh"
+echo " Copied: webSound.sh"
 cp help ~/.webSound/help
-echo " Copied  help"
-printf "\n\033[1;33m Add alias 'WebSound' to ~/.bashrc? y/N: "
+echo " Copied: help"
+printf "\n\033[1;33m Add alias: WebSound  to: ~/.bashrc  ? \n y/N: "
 read A
 if [[ $A == "y" ]]; then
-printf "\nalias WebSound=~/.webSound/./webSound.sh\n" >> ~/.bashrc
-printf "\n\033[1;33m Alias  WebSound  was added to  ~/.bashrc  and will take effect next reboot\n"
+#printf "\nalias WebSound=~/.webSound/./webSound.sh\n" >> ~/.bashrc
+printf "\033[1;33m Alias will take effect after next reboot\n"
 else
-printf "\n Did not add alias\n"
+printf " Did not add alias\n"
 fi
-printf " Script  webSound.sh  is found in  ~/.webSound/ \033[0m\n"
-printf "\n Done, you can now delete this folder\n"
+printf "\n Script: webSound.sh  -and everything else-  is found in: ~/.webSound/\033[0m\n"
+printf "\n Done, you can now delete This folder\n"
 
-printf "\n\033[1;34m  Automaticly Starts First Run\033[0m\n"
+printf "\n\033[1;34m  Automatically Starts First Run\033[0m\n"
 ~/.webSound/./webSound.sh
--- webSound.sh
+++ webSound.sh
@@ -3,45 +3,47 @@
 N=1
 Folder=~/.webSound/
 H=help
+#clear
 
+printf "\n\033[34m          WebSound\033[0m"
 while true; do
-printf "\033[2m\n---------------------------------\nEnter 'h' for help .. or \033[22m command: "
+printf "\033[2m\n----------------------------------\nEnter 'h' for help .. or \033[22m command: "
 read U A
 
-if [ $U -a ! $A ]; then
+if [ $U ]; then if [ ! $A ]; then
 
 	if [ $U == "h" ]; then
-		echo -e "\e[1;33m\n$(cat $Folder$H)\e[0m"
+		printf "\e[1;33m\n$(cat $Folder$H)\e[0m\n"
 	elif [ $U == "p" ]; then
-		echo -e "\e[1;34m  Starts Playlist $N: \e[0m\n"
+		printf "\e[1;34m  Starts Playlist $N: \e[0m\n\n"
 		mpv --vid=no --quiet $(cat $Folder$N | grep -v '#')
 	elif [ $U == "r" ]; then
-		(echo -e "\e[1;34m Playlist $N, $(cat $Folder$N | grep '#' -c) item(s)\e[0m" && cat $Folder$N | less) || echo -e "\e[31m No such playlist\e[0m"
+		(printf "\e[1;34m Playlist $N, $(cat $Folder$N | grep '#' -c) item(s)\e[0m\n" && cat $Folder$N | less) || printf "\e[31m No such playlist\e[0m\n"
 	elif [ $U == "R" ]; then
-		echo -e "\e[1;34m$(ls $Folder | egrep -iv 'websound|help')\n"
-		echo -e " Playlist $N, $(cat $Folder$N | grep '#' -c) item(s)\e[0m"
+		printf "\e[1;34m$(ls $Folder | egrep -iv 'websound|help')\n\n"
+		printf " Playlist $N, $(cat $Folder$N | grep '#' -c) item(s)\e[0m\n"
 	elif [ $U == "e" ]; then
 		nano $Folder$N
 	else
 		mpv --vid=no --quiet $U
 	fi
 
-elif [ $U -a $A == "f" ]; then
-	echo -e "\e[1;34m Found\n $(cat $Folder$N | grep -i $U)\e[0m\n"
+  elif [ $A == "f" ]; then
+	printf "\e[1;34m Found\n $(cat $Folder$N | grep -i $U)\e[0m\n\n"
 	mpv --vid=no --quiet $(cat $Folder$N | grep -i $U)
-elif [ $U -a $A == "a" ]; then
+  elif [ $A == "a" ]; then
 	printf '\n#\n' >> $Folder$N && echo $U >> $Folder$N
-	echo -e "\e[1;34m Added $URL to Playlist $N \e[0m"
-elif [ $U -a $A == "p" ]; then
-	N=$U
-	(echo -e "\e[1;34m Playlist $N, $(cat $Folder$N | grep '#' -c) item(s)\e[0m") || echo -e "\e[31m No such playlist\e[0m"
-elif [ $U -a $A == "P" ]; then
+	printf "\e[1;34m Added $URL to Playlist $N \e[0m\n"
+  elif [ $A == "p" ]; then
+	N=$U; (printf "\e[1;34m Playlist $N, $(cat $Folder$N | grep '#' -c) item(s)\e[0m\n") || printf "\e[31m No such playlist\e[0m\n"
+  elif [ $A == "P" ]; then
 	touch $Folder$U
-	echo -e "\e[1;34m Created Playlist $U \e[0m"
-elif [ $U -a $A == "C" ]; then
-	(echo -e "\e[1;34m Remove Playlist $U, with $(cat $Folder$U | grep '#' -c) item(s)?" && echo -e "\e[2;33m$(cat $Folder$U) \e[0m" && rm -i $Folder$U) || echo -e "\e[31m No such playlist found\e[0m"
-else
-	echo -e '\e[31m No proper command given\e[0m'
-fi
+	printf "\e[1;34m Created Playlist $U \e[0m\n"
+  elif [ $A == "C" ]; then
+	(printf "\e[1;34m Remove Playlist $U, with $(cat $Folder$U | grep '#' -c) item(s)\n?" && printf "\e[2;33m$(cat $Folder$U) \e[0m\n" && rm -i $Folder$U) || printf "\e[31m No such playlist found\e[0m\n"
+  fi
 
+else 	printf '\e[31m No proper command given\e[0m\n'
+
+fi
 done
