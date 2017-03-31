76185956e1700041afadfcc54da5a3912bfe5b08
mrstockis <mickem90@gmail.com>
2017-04-01 00:26:24 +0200

    added input history

--- webSound.sh
+++ webSound.sh
@@ -3,24 +3,21 @@
 N=1			##Default playlist at start
 Folder=~/.webSound/	##Location of WebSound-folder
 #clear 			##Clear terminal at start
-play="mpv --vid=no --quiet" #"--load-unsafe-playlists"	#LUP-flag fixes soundcloud refusing playback of playlist
+play="mpv --vid=no --quiet" #"--load-unsafe-playlists"	#LUP-flag fixes mpv refusing playback of playlist
 #play="cvlc some stuff"
 #############################################################
 
 #Array of color#
 declare -A c; c[E]="\033[0m"; c[d]="\033[2m"; c[D]="\033[22m"
 c[by]="\033[1;33m"; c[bb]="\033[1;34m"; c[r]="\033[31m"; c[b]="\033[34m"; c[dy]="\033[2;33m"
-c[dot]="----------------------------------"
-printf "\n${c[b]}          WebSound${c[E]}"
-while true; do
-printf "${c[d]}\n  Playlist $N: $(grep '#' -c $Folder$N) item(s)\n${c[dot]}\nEnter 'h' for help .. or ${c[D]} command: "
-read U A
-
-if [ $U ]; then clear; printf "${c[d]}${c[dot]}${c[E]}\n\n"; if [ ! $A ]; then case $U in
-		h)
-		cat "$Folder"help | less ;;
+c[dot]="---------------------------"; c[ws]=" w e b s o u n d"
+printf "\n${c[b]} ${c[ws]}${c[E]}\n"
+while printf "${c[d]}\n Playlist $N: $(grep '#' -c $Folder$N) item(s)\n${c[dot]}${c[E]}\n"; do
+read -rep "> " U A
+history -s "$U"
+if [ $U ]; then clear; printf "${c[d]}${c[ws]}${c[E]}\n\n"; if [ ! $A ]; then case $U in
 		p)
-		printf "${c[bb]}  Starts Playlist $N: ${c[E]}\n\n"
+		printf "${c[bb]} Starts Playlist $N: ${c[E]}\n\n"
 		$play $(grep -v '#' $Folder$N) 2>/dev/null  ;;
 		r)
 		cat $Folder$N 2>/dev/null || printf "${c[r]} No such playlist${c[E]}\n"  ;;
@@ -49,9 +46,9 @@ if [ $U ]; then clear; printf "${c[d]}${c[dot]}${c[E]}\n\n"; if [ ! $A ]; then c
 	K)
 	printf "${c[bb]} Remove Playlist $U, with $(grep '#' -c $Folder$U) item(s)?\n" && printf "${c[dy]}$(cat $Folder$U) ${c[E]}\n" && rm -i $Folder$U  ;;
 	*)
-	clear; printf "${c[d]}${c[dot]}\n${c[r]}\n No proper command given${c[E]}\n"  ;;
+	printf "${c[d]}${c[r]} No proper command given${c[E]}\n"  ;;
 	esac
   fi
-else 	clear; printf "${c[d]}${c[dot]}\n${c[r]}\n No proper command given${c[E]}\n"
+else	cat "$Folder"help | less
 fi
 done
