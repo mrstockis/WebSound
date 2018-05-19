c357f0446bd5f6e0c2e671ac68e9e1ad77f060a5
Mikael Malmberg <mrstockis@github.com>
2018-05-19 10:36:59 +0000

    moved 'help' to .sh file; install.sh more readable; *new*: soundcloud search, title coloring, typing history

--- install.sh
+++ install.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 clear
+
 printf "\n\033[1;34m First installation  or  Update ? \n f/u: "
 read I
+
 if [[ $I ]]; then if [[ $I == "f" ]]; then
-printf "\n\033[32m -- First Installation --\033[0m\n"
+	printf "\n\033[32m -- First Installation --\033[0m\n"
+	mkdir ~/.webSound/
+	touch ~/.webSound/demo
+	## adds three items to playlist 'demo'
+	printf "\n|Short Tunes\nhttps://www.youtube.com/watch?v=bBs-lPvk3Zk\n" >> ~/.webSound/demo
+	printf "\n|Long Tunes\nhttps://www.youtube.com/watch?v=fWRISvgAygU\n" >> ~/.webSound/demo
+	printf "\n|Radio Streams\nhttp://109.123.116.202:8010/stream\n" >> ~/.webSound/demo
+	cp webSound.sh ~/.webSound/
 
-mkdir ~/.webSound/
-touch ~/.webSound/demo
+	printf "\n Created folder: ~/.webSound/\n"
+	echo " and added execute file 'webSound.sh' and default playlist 'demo'"
 
-## adds three items to playlist 1
-printf "\n|Short Tunes\nhttps://www.youtube.com/watch?v=bBs-lPvk3Zk\n" >> ~/.webSound/demo
-printf "\n|Long Tunes\nhttps://www.youtube.com/watch?v=fWRISvgAygU\n" >> ~/.webSound/demo
-printf "\n|Radio Streams\nhttp://109.123.116.202:8010/stream\n" >> ~/.webSound/demo
+	printf "\n\033[1;34m Add alias: WebSound  to: ~/.bashrc  ? \n y/N: "
+	read A
+	if [[ $A == "y" ]]; then
+		printf "\nalias WebSound=~/.webSound/./webSound.sh\n" >> ~/.bashrc
+		printf "\n\033[1;0m Alias will take effect after next reboot\n"
+	else
+		printf "\n\033[1;0m Did not add alias\n"
+	fi
 
-cp webSound.sh ~/.webSound/
-cp help ~/.webSound/
-
-printf "\n Created folder: ~/.webSound/\n"
-echo " and added default playlist 'demo', 'webSound.sh' and 'help'"
-printf "\n\033[1;34m Add alias: WebSound  to: ~/.bashrc  ? \n y/N: "
-read A
-if [[ $A == "y" ]]; then
-printf "\nalias WebSound=~/.webSound/./webSound.sh\n" >> ~/.bashrc
-printf "\n\033[1;0m Alias will take effect after next reboot\n"
-else
-printf "\n\033[1;0m Did not add alias\n"
-fi
-printf "\n The script  -and everything else-  is found in: ~/.webSound/\n"
-printf "\n\033[1;32m  Automatically Starts First Run\033[0m\n  Enter "h" for examples on usage\n\n"
-bash ~/.webSound/webSound.sh
+	printf "\n The script  -and everything else-  is found in: ~/.webSound/\n"
+	printf "\n\033[1;32m  Automatically Starts First Run\033[0m\n  Enter "h" for examples on usage\n\n"
+	bash ~/.webSound/webSound.sh
 
 elif [[ $I == "u" ]]; then
-printf "\n\033[1;32m -- Update --\033[0m\n"
-cp webSound.sh ~/.webSound/
-printf "\n Updated: webSound.sh\n"
-cp help ~/.webSound/
-printf " Updated: help\n"
-printf "\n\033[1;34m Done\n\n\033[0m"
+	printf "\n\033[1;32m -- Update --\033[0m\n"
+	cp webSound.sh ~/.webSound/
+	printf "\n Updated: webSound.sh\n"
+	printf "\n\033[1;34m Done\n\n\033[0m"
 
 else
-printf "\n Input not recognized. Enter 'f' for First intallation, or 'u' for Update.\n\n Exits.. \n\n"
+	printf "\n Input not recognized. Enter 'f' for First intallation, or 'u' for Update.\n\n Exits.. \n\n"
 fi
 else
-printf "\n No input given. Enter 'f' for First intallation, or 'u' for Update.\n\n Exits.. \n\n"
+	printf "\n No input given. Enter 'f' for First intallation, or 'u' for Update.\n\n Exits.. \n\n"
 fi
--- webSound.sh
+++ webSound.sh
@@ -12,25 +12,49 @@ play="mpv --vid=no --really-quiet" #" --load-unsafe-playlists"	##LUP-flag fixes 
 
 declare -A c; c[E]="\033[0m"; c[d]="\033[2m"; c[D]="\033[22m"
 	c[by]="\033[1;33m"; c[bb]="\033[1;34m"; c[r]="\033[31m"; c[b]="\033[34m"; c[dy]="\033[2;33m"
-	c[dot]="---------------------------"; c[ws]=" w e b s o u n d"
+	c[ws]=" w e b s o u n d"; c[yt]=" y o u t u b e"; c[sc]=" s o u n d c l o u d"
+	c[dot]="---------------------------"
 
 printf "${c[b]}${c[ws]}${c[E]}\n"
 
+help=(" SYNTAX: [First] (Second)"
+""
+" [h]	→  This help"
+""
+" [URL]	→  Play [URL]"
+" [y]	→  Search youtube"
+" [s]	→  Search soundcloud"
+" [p]	→  Play through active playlist"
+" [r]	→  Read current playlist"
+" [l]	→  List all playlists"
+" [e]	→  Edit playlist"
+""
+" [URL] (a)	  → (a)dd [URL] to current playlist"
+" [SearchTerm] (p) → (p)lay [SearchTerm] from playlist"
+" [ListName] (l)	→  choose playlist [ListName]"
+" [ListName] (L)	→  create playlist [ListName]"
+" [ListName] (K)	→  delete playlist [ListName]"
+""
+" <q> exits active media <ctrl+c> stops script"
+""
+" Some preferences(player,editor,etc.) can be made "
+" by editing top-section of the script '.webSound.sh'")
 
 function Add() {
 	E=$(printf "$(youtube-dl --flat-playlist -e "$1")\n" | head -n 1)
-	
+
 	if [ ! "$E" ]; then E="Playlist?"; fi
-	
+
 	printf "\n|$E\n"$1"\n" >> $Folder$N
 	printf "${c[b]} Added:  $E\n    To:  $N ${c[E]}\n"
 }
 
 function Ysearch() {
-	while true; do read -rep ' Search: ' s
-		
+	while true; do clear; printf "${c[b]}${c[r]}${c[yt]}${c[E]}\n\n"
+		read -rep ' Search: ' s
+		history -s "$s"
+
 		if [ ! "$s" ]; then echo ' ...'; break
-		
 		else
 			S="$(echo "$s" | cut -f1- -d" " --output-delimiter="+")"
 			w3m -dump -o display_link_number=1 https://www.youtube.com/results?search_query="$S" |
@@ -38,27 +62,62 @@ function Ysearch() {
 			grep "\[" |
 			sed 's/[ \t]*//' |
 			head -n "$YT"
-		
+
 		while true; do read -rep ' Play or add: ' P A
-			
+
 			if [ ! "$P" ]; then break
-			
+
 			elif [ ! "$A" ]; then
 				mpv --vid=no --really-quiet $(
 				w3m -dump -o display_link_number=1 https://www.youtube.com/results?search_query="$S" |
-				grep "References:" -A 200 | 
+				grep "References:" -A 200 |
 				grep "\["$P"\]" |
 				awk '{print $2}')
-			
+
 			elif [ "$A" == "a" ]; then
 				Add "$(w3m -dump -o display_link_number=1 https://www.youtube.com/results?search_query="$S" |
 				grep "References:" -A 200 |
 				grep "\["$P"\]" |
 				awk '{print $2}')"
-	
+
 			else
 				printf " [Number]	→ play choice\n [Number] a	→ add choice\n [empty]	→ exit\n"
-	fi; done; fi; done
+
+			fi; done; fi; done
+}
+
+function SCsearch(){
+	while true; do clear; printf "${c[dy]}${c[sc]}${c[E]}\n\n"
+		read -rep ' Search: ' s
+		history -s "$s"
+
+		if [ ! "$s" ]; then echo ' ...'; break
+		else
+			S=`echo "$s" | cut -f1 -d" " --output-delimiter='%20'`
+			w3m -dump -o display_link_number=1 https://soundcloud.com/search?q=$S |
+			grep "\[7\]" -A 30 | head -n 20
+
+		while true; do read -rep ' Play or add: ' P A
+
+			if [ ! "$P" ]; then break
+
+			elif [ ! "$A" ]; then
+				mpv --vid=no --really-quiet `
+				w3m -dump -o display_link_number=1 https://soundcloud.com/search?q="$S" |
+				grep "References:" -A 200 |
+				grep "\["$P"\]" |
+				awk '{print $2}'`
+
+			elif [ "$A" == "a" ]; then
+				Add `w3m -dump -o display_link_number=1 https://soundcloud.com/search?q="$S" |
+				grep "References:" -A 200 |
+				grep "\["$P"\]" |
+				awk '{print $2}'`
+
+			else
+				printf " [Number]	→ play choice\n [Number] a	→ add choice\n [empty]	→ exit\n"
+
+			fi; done; fi; done
 }
 
 
@@ -68,15 +127,17 @@ while printf "${c[d]}\n $N{$(grep '|' -c $Folder$N)}\n${c[dot]}${c[E]}\n"; do
 	history -s "$U"
 
 	if [ $U ]; then clear; printf "${c[d]}${c[b]}${c[ws]}${c[E]}\n\n"
-		
+
 		if [ ! $A ]; then
-			
+
 			case $U in
-				h)	cat "$Folder"help | less; clear; printf "${c[d]}${c[b]}${c[ws]}${c[E]}\n" ;;
-				
+				h)	for i in "${help[@]}"; do printf "\033[1m$i${c[E]}\n"; done | less -r; clear; printf "${c[d]}${c[b]}${c[ws]}${c[E]}\n";; 
+
 				y)	Ysearch  ;;
-				
-				p)	printf "${c[b]} Starts Playlist $N: ${c[E]}\n"
+
+				s)	SCsearch ;;
+
+				p)	printf "${c[b]} Play: $N: ${c[E]}\n"
 					printf "${c[b]}$(grep '|' $Folder$N | cut -d"|" -f1-)${c[E]}\n"
 					$play $(grep -v '|' $Folder$N) 2>/dev/null  ;;
 
@@ -88,14 +149,14 @@ while printf "${c[d]}\n $N{$(grep '|' -c $Folder$N)}\n${c[dot]}${c[E]}\n"; do
 					done  ;;
 
 				e)	$editor $Folder$N  ;;
-	
+
 				*)	E=$(youtube-dl --flat-playlist -e $U 2>/dev/null); printf "${c[b]}"
 					if [ ! "$E" ]; then echo " Play"; else echo " $E"; fi
 					printf "${c[E]}\n"; $play $U 2>/dev/null  ;;
 			esac
-	
+
 	elif [ $A ]; then case $A in
-	
+
 		p)	if [ ! "$(grep -i $U $Folder$N | grep '|')" ]; then
 				printf "${c[b]}$(grep -i $U $Folder$N)${c[E]}\n\n"
 				$play $(grep -i $U $Folder$N) 2>/dev/null
@@ -104,20 +165,23 @@ while printf "${c[d]}\n $N{$(grep '|' -c $Folder$N)}\n${c[dot]}${c[E]}\n"; do
 				printf "${c[b]}$(grep -i $U $Folder$N)${c[E]}\n\n"
 				$play ${f[@]} 2>/dev/null
 			fi  ;;
-		
+
 		a)	Add "$U"  ;;
 
 		l)	N=$U  ;;
-		
+
 		L)	touch $Folder$U; N=$U
 			printf "${c[b]} Created Playlist $U ${c[E]}\n"  ;;
-		
+
 		K)	printf "${c[b]} Remove Playlist $U, with $(grep '|' -c $Folder$U) item(s)?\n"
 			printf "${c[dy]}$(cat $Folder$U) ${c[E]}\n" && rm -i $Folder$U  && N=$n ;;
-		
+
 		*)	printf "${c[d]}${c[r]} No proper command given${c[E]}\n"  ;;
-		
+
 	esac; fi
 
 else clear; printf "${c[d]}${c[b]}${c[ws]}${c[E]}\n"
 fi; done
+
+
+
